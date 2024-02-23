02d14ef93e08a0993181dc170a7a73ab0c60fe4c
NoteBook <fatmaahmed0008@gmail.com>
2024-02-23 04:46:20 +0200

    data_second_commit

--- lib/features/register_and_login/presentation/view/login_screen.dart
+++ lib/features/register_and_login/presentation/view/login_screen.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-
-import '../../../../core/components/componenets.dart';
-import '../../../../core/components/constants.dart';
-import '../../../../general/widgets/custom-text.dart';
 import '../widgets/login_screen_body.dart';
 
 class LoginScreen extends StatelessWidget {
